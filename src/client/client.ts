6ad86a205438c3f8d5de1e40d18a276450457bef
i2phi <jfiturbe@gmail.com>
2025-04-12 10:18:05 -0500

    temporary remove node modules, added gitignore, working mcp example

--- src/client/client.ts
+++ src/client/client.ts
@@ -2,7 +2,7 @@ const { Client } = await import("@langchain/langgraph-sdk");
 const client = new Client({ apiUrl: "http://localhost:2024" });
 const assistantID = "tool-router";
 const thread = await client.threads.create();
-console.log(thread);
+//console.log(thread);
 
 const streamResponse = client.runs.stream(thread["thread_id"], assistantID, {
   input: {
@@ -11,8 +11,6 @@ const streamResponse = client.runs.stream(thread["thread_id"], assistantID, {
   streamMode: "messages",
 });
 for await (const chunk of streamResponse) {
-  console.log(`Receiving new event of type: ${chunk.event}...`);
   console.log(chunk.data);
-  console.log("\n\n");
 }
 export default client;
